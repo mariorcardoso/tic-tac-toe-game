908715a2b71dd9e4a5c802c39da79e47efdfbca2
Mario Cardoso <cardosomarioar@gmail.com>
2017-04-23 16:52:49 +0100

    when someone wins, highlight the three squares that caused the win

--- src/index.js
+++ src/index.js
@@ -4,15 +4,23 @@ import './index.css';
 
 function Square(props) {
     return (
-      <button className="square" onClick={() => props.onClick()}>
+      <button className={props.winner ? "square winner" : "square"} onClick={() => props.onClick()}>
         {props.value}
       </button>
     );
 }
 
 class Board extends React.Component {
+  winner(i){
+    if (this.props.winner) {
+      return this.props.winner.includes(i);
+    } else {
+      return false;
+    }
+  }
+
   renderSquare(i) {
-    return <Square key={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
+    return <Square key={i} winner={this.winner(i)} value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
   }
   renderRow(indexes, row_index) {
     const row = indexes.map((index) => {
@@ -110,6 +118,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winner={winner}
           />
         </div>
         <div className="game-info">
@@ -144,7 +153,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return lines[i];
     }
   }
   return null;
